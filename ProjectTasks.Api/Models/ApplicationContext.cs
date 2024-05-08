afc82d0e3466a9224ecd7cab6f70201f85c974bd
Dmitry Romashov <framebassman@gmail.com>
2024-05-08 10:32:46 +0200

    Fix

--- ProjectTasks.Api/Models/ApplicationContext.cs
+++ ProjectTasks.Api/Models/ApplicationContext.cs
@@ -7,5 +7,7 @@ public class ApplicationContext : DbContext
     public DbSet<Project> Projects { get; set; }
     public DbSet<Task> Tasks { get; set; }
 
-    public ApplicationContext(DbContextOptions options) : base(options) {}
+    public ApplicationContext(DbContextOptions options) : base(options)
+    {
+    }
 }
