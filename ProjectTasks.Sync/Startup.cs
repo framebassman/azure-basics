9dddc8b0e944d8445fbbbe5180187906f49f28d0
Dmitry Romashov <framebassman@gmail.com>
2024-06-22 16:04:06 +0200

    Add a templat

---
+++ ProjectTasks.Sync/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: FunctionsStartup(typeof(ProjectTasks.Sync.Startup))]
+namespace ProjectTasks.Sync
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+        }
+    }
+}
