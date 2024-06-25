53991e3dfa10cde55a9b8604cfdf93c672caaddc
Dmitry Romashov <framebassman@gmail.com>
2024-06-25 11:08:59 +0200

    Get all unsync projects

--- ProjectTasks.Sync/MyHttpTrigger.cs
+++ ProjectTasks.Sync/MyHttpTrigger.cs
@@ -2,22 +2,27 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ProjectTasks.Sync.Model.Sql;
 
 namespace ProjectTasks.Sync
 {
     public class MyHttpTrigger
     {
         private readonly ILogger<MyHttpTrigger> _logger;
+        private SqlContext _sql;
 
-        public MyHttpTrigger(ILogger<MyHttpTrigger> logger)
+        public MyHttpTrigger(ILogger<MyHttpTrigger> logger, SqlContext sql)
         {
             _logger = logger;
+            _sql = sql;
         }
 
         [Function("MyHttpTrigger")]
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+            var projects = _sql.UnsyncronizedProjects.ToList();
+            _logger.LogInformation("{@projects}", projects);
             return new OkObjectResult("Welcome to Azure Functions!");
         }
     }
