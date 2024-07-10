626f86dbaee6f4221c237948ec1916b35dc4fb58
Dmitry Romashov <framebassman@gmail.com>
2024-07-10 22:48:20 +0200

    Remove Tables.WebApi project

--- ProjectTasks.Tables.WebApi/Models/ProjectRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace ProjectTasks.Tables.WebApi.Models;
-
-public class ProjectRequest
-{
-    public string Name { get; set; }
-    public string Code { get; set; }
-}
--- ProjectTasks.Tables.WebApi/Models/ProjectResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace ProjectTasks.Tables.WebApi.Models;
-
-public class ProjectResponse
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Code { get; set; }
-}
